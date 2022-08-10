8246433da8eacb03be3d2a3a7db9bc27f72d5c46
akmalamka <akmalmuhammad51@gmail.com>
2022-08-10 13:43:04 +0700

    fix: change back withous es6 module

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-import 'dotenv/config'
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import * as prismic from '@prismicio/client'
-import * as prismicH from '@prismicio/helpers'
-import * as prismicDOM from 'prismic-dom'
-import { client } from './src/config/prismicConfig.js'
+require('dotenv').config()
+
+const express = require('express')
+const path = require('path')
+const prismic = require('@prismicio/client')
+const prismicDOM = require('prismic-dom')
+const prismicH = require('@prismicio/helpers')
+// const { default: fetch } = require('node-fetch')
+const fetch = (...args) =>
+	import('node-fetch').then(({ default: fetch }) => fetch(...args))
 
 const app = express()
 const port = 3000
@@ -18,19 +20,56 @@ app.use((req, res, next) => {
 	next()
 })
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const repoName = 'akmalportfolio'
+const accessToken = process.env.PRISMIC_ACCESS_TOKEN
+
+const routes = [
+	{
+		type: 'home',
+		path: '/',
+	},
+	{
+		type: 'about',
+		path: '/about',
+	},
+	{
+		type: 'work',
+		path: '/works',
+	},
+]
+
+const client = prismic.createClient(repoName, {
+	fetch,
+	accessToken,
+	routes,
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.get('/', async (req, res) => {
-	const document = await client.getSingle('home')
-	console.log(document)
+	// client
+	// 	.get(prismic.predicate.at('document.type', 'about'))
+	// 	.then((response) => {
+	// 		console.log(response.results)
+	// 		res.render('pages/home')
+	// 	})
+	// client.getSingle('home').then((response) => {
+	// 	console.log(response.data)
+	// 	res.render('pages/home')
+	// })
+	// const document = await client.getSingle('home')
+	// console.log(document)
 	res.render('pages/home')
-	// res.render('pages/home')
 })
 
 app.get('/about', (req, res) => {
-	res.render('pages/about')
+	client
+		.get(prismic.predicate.at('document.type', ['about', 'meta']))
+		.then((response) => {
+			console.log('Response', response)
+			res.render('pages/about')
+		})
 })
 
 app.get('/works', (req, res) => {
