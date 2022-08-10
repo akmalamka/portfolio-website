14a7fab5033cf5cbdf10f19d33d3b137ce244cb8
akmalamka <akmalmuhammad51@gmail.com>
2022-08-10 10:29:51 +0700

    feat: add client for fetching data from prismic

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const express = require('express')
+import 'dotenv/config'
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import * as prismic from '@prismicio/client'
+import * as prismicH from '@prismicio/helpers'
+import * as prismicDOM from 'prismic-dom'
+import { client } from './src/config/prismicConfig.js'
+
 const app = express()
-const path = require('path')
 const port = 3000
 
+app.use((req, res, next) => {
+	res.locals.ctx = {
+		prismicH,
+	}
+	// res.locals.prismicDOM = prismicDOM
+	next()
+})
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-app.get('/', (req, res) => {
+
+app.get('/', async (req, res) => {
+	const document = await client.getSingle('home')
+	console.log(document)
 	res.render('pages/home')
+	// res.render('pages/home')
 })
 
 app.get('/about', (req, res) => {
@@ -17,7 +37,7 @@ app.get('/works', (req, res) => {
 	res.render('pages/works')
 })
 
-app.get('/works/:id', (req, res) => {
+app.get('/works/:uid', (req, res) => {
 	res.render('pages/blog')
 })
 
