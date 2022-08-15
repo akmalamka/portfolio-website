ed9ab1a78b298ad1287a1a549c6d017332e5460c
akmalamka <akmalmuhammad51@gmail.com>
2022-08-15 13:45:06 +0700

    feat: scss structure for styling

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride())
 app.use(errorHandler())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // const handleLinkResolver = (doc) => {
 // 	if (doc.type === 'blog') {
